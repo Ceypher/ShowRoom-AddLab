5f051aed9807e1b557444f8f01c44797cad8de29
Ceypher <ceypher@gmail.com>
2016-11-21 17:27:13 +0100

    Update WifiSingleLampController.ino

--- WIFI LAMP CONTROLLER/FIRMWARE ESP-01/WifiSingleLampController/WifiSingleLampController.ino
+++ WIFI LAMP CONTROLLER/FIRMWARE ESP-01/WifiSingleLampController/WifiSingleLampController.ino
@@ -9,92 +9,115 @@
 
 #include <ESP8266WiFi.h>
 
-const char* ssid = "ssid";
-const char* password = "pass";
-IPAddress ip(192, 168, 43, 30);
-IPAddress gateway(192, 168, 1, 1); // set gateway to match your network
-IPAddress subnet(255, 255, 255, 0); // set subnet mask to match your
+const char* ssid = "toto";
+const char* password = "tatatata";
 
 // Create an instance of the server
 // specify the port to listen on as an argument
 WiFiServer server(80);
 
 void setup() {
-  Serial.begin(115200);
+  Serial.begin(9600);
   delay(10);
 
-// prepare GPIO
+  // prepare GPIO
+  pinMode(0, OUTPUT);
   pinMode(2, OUTPUT);
-  digitalWrite(2, 0);
+  digitalWrite(0, HIGH);
+  digitalWrite(2, HIGH);
   
-//Disconnect WiFi
-  WiFi.disconnect();
-  WiFi.softAPdisconnect();
-  WiFi.mode(WIFI_OFF);
-  delay(500);
-
-//Set WiFi in static IP mode
-  WiFi.mode(WIFI_STA);
-  WiFi.config(ip,gateway,subnet);
-
-//Connect to AP
-  Serial.println();
-  Serial.println();
-  Serial.print("Connecting to ");
-  Serial.println(ssid);
+  // Connect to WiFi network
+//  Serial.println();
+//  Serial.println();
+//  Serial.print("Connecting to ");
+//  Serial.println(ssid);
+  
   WiFi.begin(ssid, password);
   
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
-    Serial.print(".");
+//    Serial.print(".");
   }
-  Serial.println("");
-  Serial.println("WiFi connected");
+//  Serial.println("");
+//  Serial.println("WiFi connected");
   
-// Start the server
+  // Start the server
   server.begin();
-  Serial.println("Server started");
+//  Serial.println("Server started");
 
-// Print the IP address
-  Serial.println(WiFi.localIP());
+  // Print the IP address
+//  Serial.println(WiFi.localIP());
 }
 
 void loop() {
-// Check if a client has connected
+  // Check if a client has connected
   WiFiClient client = server.available();
   if (!client) {
     return;
   }
   
-// Wait until the client sends some data
-  Serial.println("new client");
+  // Wait until the client sends some data
+//  Serial.println("new client");
   while(!client.available()){
     delay(1);
   }
   
-// Read the first line of the request
-  String req = client.readStringUntil('\r');
-  Serial.println(req);
+  // Read the first line of the request
+  String req = client.readStringUntil(' ');
+  req = client.readStringUntil(' ');
+//  Serial.println(req);
   client.flush();
   
-// Match the request and set GPIO
-  if (req.indexOf("/LED0") != -1) digitalWrite(2, LOW);
-  if (req.indexOf("/LED1") != -1) digitalWrite(2, HIGH);
-  
+  // Match the request
+  if      (req.indexOf("/leds/01/0") != -1)
+  {
+    digitalWrite(0,HIGH);
+  }
+  else if (req.indexOf("/leds/01/1") != -1)
+  {
+    digitalWrite(0,LOW);
+  }
+  else if (req.indexOf("/leds/02/0") != -1)
+  {
+    digitalWrite(2,HIGH);
+  }
+  else if (req.indexOf("/leds/02/1") != -1)
+  {
+    digitalWrite(2,LOW);
+  }
+  else if (req.indexOf("/leds/all/1") != -1)
+  {
+    digitalWrite(0,LOW);
+    digitalWrite(2,LOW);
+    Serial.println(req);
+  }
+  else if (req.indexOf("/leds/all/0") != -1)
+  {
+    digitalWrite(0,HIGH);
+    digitalWrite(2,HIGH);
+    Serial.println(req);
+  }
+  else if (req.indexOf("/leds/") != -1)
+  {
+    Serial.println(req);
+  }
+  else {
+    //Serial.println("invalid request");
+    client.stop();
+    return;
+  }
+
   client.flush();
 
   // Prepare the response
-  String s = "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n<!DOCTYPE HTML>\r\n<html>\r\n";
-  s += "<h1><a href='/LED0'>Switch off LED</a></h1>";
-  s += "<h1><a href='/LED1'>Switch on LED</a></h1>";
+  String s = "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n<!DOCTYPE HTML>\r\n<html>OK ";
   s += "</html>\n";
 
   // Send the response to the client
   client.print(s);
   delay(1);
-  Serial.println("Client disonnected");
+//  Serial.println("Client disonnected");
 
   // The client will actually be disconnected 
   // when the function returns and 'client' object is detroyed
 }
-
